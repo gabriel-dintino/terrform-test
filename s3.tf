b127fc58f7e4091dd7b20f1d855c578988da34cd
Gabriel D'Intino <GDintino@proveedor.teco.com.ar>
2019-05-03 18:04:17 -0300

    Siguiendo el training.

--- s3.tf
+++ s3.tf
@@ -1,8 +1,13 @@
 resource "aws_s3_bucket" "main" {
-    bucket = "packt-terraform-section2-bucket"
+    bucket = "${var.s3_bucket_name}"
     acl = "private"
+
     tags {
+        /*
         Name = "my Bucket"
         Environmet = "Dev"
+        */
+        env = "${lookup(var.s3_tags, "environment")}"
     }
+    region = "${var.s3_regions[0]}"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+s3_bucket_name = "packt-terraform-section2-bucket"
